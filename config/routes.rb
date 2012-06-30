753584f9d328c93d68f5db97a0cc14af1830edf8
Rubén Bernárdez <rubenbp@gmail.com>
2012-06-30 17:06:48 +0200

    Proxy del API de karmacracy para ranking y datos de un usuario

--- config/routes.rb
+++ config/routes.rb
@@ -55,4 +55,11 @@ Karmafighters::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
+
+  namespace :api do
+    namespace :v1 do
+      get "rank" => "api-proxy#rank", :as => :rank
+      get "user/:username" => "api-proxy#user", :as => :user
+    end
+  end
 end
